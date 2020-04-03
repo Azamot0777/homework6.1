199552fc812b2f1b289aede4ac3730cd8c679d7d
Heisenberg <Persey_98@list.ru>
2020-04-03 19:38:23 +0500

    created class

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Homework6
+{
+
+
+    class Rectangle
+    {
+        public double side1 { get; set; }
+        public double side2 { get; set; }
+
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
