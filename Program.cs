d295d88bb6d19be8f66a7f991d91d575b928e2e9
Heisenberg <Persey_98@list.ru>
2020-04-03 21:53:39 +0500

    fix bugs

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace Homework6
         public double side1 { get; set; }
         public double side2 { get; set; }
 
-        public double Area { get; set; }
+        public double area { get; set; }
 
         public double Perimetr { get; set; }
 
@@ -21,27 +21,18 @@ namespace Homework6
         }
         public double AreaCalculator(double side1, double side2)
         {
-            Area = side1 * side2;
-            return Area;
+            area = side1 * side2;
+            return area;
         }
 
-        public double Perimetr(double side1, double side2)
+        public double PerimetrCalculator(double side1, double side2)
         {
             Perimetr = (side1 + side2) * 2;
             return Perimetr;
         }
 
-        public void GetInfo()
-        {
-
-        }
-
-        public void PerimetrCalculator(double)
-        {
-
-        }
-
-
+        public double Area { get { return Area; } }
+        public double Perimetr {get { return Perimetr; } }
 
     }
 
@@ -51,8 +42,14 @@ namespace Homework6
         static void Main(string[] args)
         {
             Rectangle s = new Rectangle();
+            System.Console.WriteLine("Side 1 = ");
             s.side1 = double.Parse(Console.ReadLine());
+            System.Console.WriteLine("Side 2 = ");
             s.side2 = double.Parse(Console.ReadLine());
+            s.AreaCalculator(side1, side2);
+            System.Console.WriteLine($"Area is {s.Area}");
+            s.PerimetrCalculator(side1, side2);
+            System.Console.WriteLine($"Perimetr is {s.Perimetr}");
         }
     }
 }
