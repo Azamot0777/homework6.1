926ae09e7e3ce68ad78fa6d061e476da8a09b7b6
Heisenberg <Persey_98@list.ru>
2020-04-03 20:39:49 +0500

    created AreaCalculator and Perimetr

--- Program.cs
+++ Program.cs
@@ -9,11 +9,29 @@ namespace Homework6
         public double side1 { get; set; }
         public double side2 { get; set; }
 
-        public double Area { get; }
+        public double Area { get; set; }
 
-        public double Perimetr { get; }
+        public double Perimetr { get; set; }
 
-        public void AreaCalculator(double)
+        public Init() { }
+        public Init(double side1, double side2)
+        {
+            this.side1 = side1;
+            this.side2 = side2;
+        }
+        public double AreaCalculator(double side1, double side2)
+        {
+            Area = side1 * side2;
+            return Area;
+        }
+
+        public double Perimetr(double side1, double side2)
+        {
+            Perimetr = (side1 + side2) * 2;
+            return Perimetr;
+        }
+
+        public void GetInfo()
         {
 
         }
